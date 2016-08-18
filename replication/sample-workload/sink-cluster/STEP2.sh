4c487dce3ba4a32b30df657847452babe82dbf13
Ubuntu <nitinver@hn0-hbrepl.nbm4qjtvplzurpujgyakqoau0c.cx.internal.cloudapp.net>
2016-08-18 00:31:37 +0000

    Dropping only relevant tables in sample workload.

--- replication/sample-workload/sink-cluster/STEP2.sh
+++ replication/sample-workload/sink-cluster/STEP2.sh
@@ -1,25 +1,15 @@
 #!/bin/bash 
 
-echo "Dropping all tables shown below... "
+echo "Dropping test1 and test2 tables... "
 
-curl -u admin:Had00p\!123 -G "https://hbrpl2.azurehdinsight.net/hbaserest/" 2> /dev/null
-
-echo "" > /tmp/workload.txt
-
-for user_table in `curl -u admin:Had00p\!123 -G "https://hbrpl2.azurehdinsight.net/hbaserest/"`
-do
-cat << ... >> /tmp/workload.txt
-	disable '$user_table'
-	drop 	'$user_table'
-...
-done
-
-cat << ... >> /tmp/workload.txt
-	list
-	exit
+hbase shell << ... 2> /dev/null
+disable 'test1'
+drop    'test1'
+disable 'test2'
+drop    'test2'
+list
+exit
 ...
 
-echo "Doing following operations ..."
-cat /tmp/workload.txt
 
-hbase shell /tmp/workload.txt 2> /dev/null
+
