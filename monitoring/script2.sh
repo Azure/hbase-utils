215211bac7233c2be734fdbe30fb0ad1d7512b03
duoxu <duoxu@microsoft.com>
2016-10-06 16:58:27 -0700

    add yarn conf as part of installation

--- monitoring/script2.sh
+++ monitoring/script2.sh
@@ -3,8 +3,12 @@
 wget https://github.com/Microsoft/OMS-Agent-for-Linux/releases/download/OMSAgent_GA_v1.2.0-25/omsagent-1.2.0-25.universal.x64.sh -O /tmp/omsagent.x64.sh
 sudo sh /tmp/omsagent.x64.sh --upgrade
 
-if [[ $HOSTNAME == wn* ]];
+if [[ $HOSTNAME == hn* ]];
 then
+  sudo wget https://raw.githubusercontent.com/Azure/hbase-utils/master/monitoring/yarn.headnode.conf -O /etc/opt/microsoft/omsagent/conf/omsagent.d/yarn.headnode.conf
+elif [[ $HOSTNAME == wn* ]];
+then
+  sudo wget https://raw.githubusercontent.com/Azure/hbase-utils/master/monitoring/yarn.workernode.conf -O /etc/opt/microsoft/omsagent/conf/omsagent.d/yarn.workernode.conf
   sudo wget https://raw.githubusercontent.com/Azure/hbase-utils/master/monitoring/hbase.workernode.conf -O /etc/opt/microsoft/omsagent/conf/omsagent.d/hbase.workernode.conf
 elif [[ $HOSTNAME == zk* ]];
 then
