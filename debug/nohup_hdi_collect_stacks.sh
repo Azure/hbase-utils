3db618550a36e100783e93befc69f1d074a69965
nitinver <nitin.verma@gmail.com>
2016-10-27 18:01:37 +0000

    Fixing nohup operation

--- debug/nohup_hdi_collect_stacks.sh
+++ debug/nohup_hdi_collect_stacks.sh
@@ -4,4 +4,4 @@ wget https://raw.githubusercontent.com/Azure/hbase-utils/master/debug/hdi_collec
 
 chmod +x /tmp/hdi_collect_stacks.sh
 
-nohup /tmp/hdi_collect_stacks.sh $@ > /var/log/hbase/hdi_collect_stacks.log 2>&1
+nohup bash /tmp/hdi_collect_stacks.sh $@ &
