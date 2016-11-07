b39923d2a0dafc5a828acf6c47392908feacc9c2
nitinver <nitin.verma@gmail.com>
2016-11-07 23:53:38 +0000

    Removing STEP3.cmd file

--- replication/STEP3.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Enabling replication on all the tables..."
-sleep 3
-bash ./hdi_enable_replication.sh -s hbrepl1 -d hbrpl2 -sp 'Had00p\!123' -dp Had00p\!123 -t "test1;test2" -cp
