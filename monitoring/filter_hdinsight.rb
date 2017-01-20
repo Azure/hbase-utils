2b36a2960f79c14ae260a018da492ef53dc133d4
duoxu <duoxu@microsoft.com>
2017-01-19 16:08:40 -0800

    add cluster type

--- monitoring/filter_hdinsight.rb
+++ monitoring/filter_hdinsight.rb
@@ -12,21 +12,26 @@ module Fluent
     RUBY_DIR = BASE_DIR + '/ruby/bin/ruby '
     SCRIPT = BASE_DIR + '/bin/hdinsightmanifestreader.rb '
 
-  def configure(conf)
+        attr_accessor :command
+
+    def configure(conf)
       super
-      @hostname = OMS::Common.get_hostname or "Unknown host"
+      @hostname = OMS::Common.get_hostname or "unknown"
       @command = "sudo " << RUBY_DIR << SCRIPT
       @clustername = ""
-      Open3.popen3(@command) {|stdin, stdout, stderr, wait_thr|
-          pid = wait_thr.pid
-          stdin.close
-          @clustername = stdout.read
-          wait_thr.value
-      }
+      @clustertype = ""
     end
 
     def start
       super
+      Open3.popen3(@command) {|stdin, stdout, stderr, wait_thr|
+          pid = wait_thr.pid
+          stdin.close
+          parsed = JSON.parse(stdout.read)
+          @clustername = parsed["cluster_name"]
+          @clustertype = parsed["cluster_type"]
+          wait_thr.value
+      }
     end
 
     def shutdown
@@ -36,6 +41,7 @@ module Fluent
     def filter(tag, time, record)
       record["ClusterName"] = @clustername
       record["HostName"] = @hostname
+      record["ClusterType"] = @clustertype
       record
     end
   end
