e0eb7d0fe9751338368c8c0670c77da419caf8bc
gkanade <gaurav.kanade@gmail.com>
2017-08-07 14:29:00 -0700

    Clear regions in transition: Added hbck check post operation

--- scripts/clearregionintransitionznode.sh
+++ scripts/clearregionintransitionznode.sh
@@ -1,15 +1,24 @@
 #/bin/bash
-echo "ls /hbase/region-in-transition"|hbase zkcli
 result=$(echo "rmr /hbase/region-in-transition"|hbase zkcli 2>&1 | grep "Node does not exist:")
-echo $result
 if [[ -z $result ]]; then
-    echo "Regions stuck in transition successfully removed, please retry hbase hbck command"
+    echo "Regions stuck in transition successfully removed, running hbck to confirm"
+    hbckresult=$(hbase hbck -ignorePreCheckPermission | grep "0 inconsistencies detected")
+    if [[ ! -z $hbckresult ]]; then
+        echo "HBase is in good health"
+    else
+        echo "Could not confirm hbase status; please check by running hbck manually"
+    fi
 else
-   echo "ls /hbase-unscure/region-in-transition"|hbase zkcli
     result=$(echo "rmr /hbase-unsecure/region-in-transition"|hbase zkcli 2>&1 | grep "Node does not exist:")
     if [[ -z $result ]]; then
-        echo "Regions stuck in transition successfully removed, please retry hbase hbck command"
+        echo "Regions stuck in transition successfully removed, running hbase hbck to confirm"
+        hbckresult=$(hbase hbck -ignorePreCheckPermission | grep "0 inconsistencies detected")
+        if [[ ! -z $hbckresult ]]; then
+            echo "HBase is in good health"
+        else
+            echo "Could not confirm hbase status; please check by running hbck manually"
+        fi        
     else
-        echo "Operation failed; please contact support if you need more assistance"
+        echo "No entries in found corresponding to region-in-transition znode; please contact support if you need further assistance"
     fi
 fi
